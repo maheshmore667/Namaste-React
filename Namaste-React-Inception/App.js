9047bd1fbf25b323a334f35a3eba00d568525f42
Mahesh More <maheshmore667@gmail.com>
2023-11-19 00:34:59 +0530

    Inception folder with react work

---
+++ Namaste-React-Inception/App.js
@@ -0,0 +1,41 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Namste React by Mahesh!"
+);
+
+//nested element creation
+const nestedElements = React.createElement(
+  "div",
+  { id: "parent" },
+  [React.createElement(
+    "div",
+    { id: "child1" },
+    [React.createElement(
+      "h1",
+      { id: "nestedheading1" },
+      "Namste React by Mahesh of nested elements!"
+    ),React.createElement(
+        "h2",
+        { id: "nestedheading2" },
+        "Namste React by Mahesh of nested elements!"
+      )]
+  ),
+  React.createElement(
+    "div",
+    { id: "child2" },
+    [React.createElement(
+      "h1",
+      { id: "nestedheading3" },
+      "Namste React by Mahesh of nested elements!"
+    ),React.createElement(
+        "h2",
+        { id: "nestedheading4" },
+        "Namste React by Mahesh of nested elements!"
+      )]
+  )
+]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(nestedElements);
