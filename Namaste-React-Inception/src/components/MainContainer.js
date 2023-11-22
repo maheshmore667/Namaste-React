a443b62b44c91c57494b6add8f0f8db4b20e3ad0
Mahesh More <maheshmore667@gmail.com>
2023-11-23 00:40:31 +0530

    Use state hook is used + code is cleaned

--- Namaste-React-Inception/src/components/MainContainer.js
+++ Namaste-React-Inception/src/components/MainContainer.js
@@ -1,18 +1,25 @@
 import RestaurantCard from "./RestaurantCard";
-import { restList } from "../Utils/mockData";
-
-  
+import { restList as restListData } from "../Utils/mockData";
+import { useState } from "react";
 
 const MainContainer = () => {
-    return (
+  const [restList, setRestList] = useState(restListData);
+  const filterRestList = () => {
+    setRestList(restList?.filter((res)=> res?.info?.avgRating > 4));
+  }
+  return (
+    <div className="body-content">
+      <button className="btn top-res" onClick={filterRestList}>Top Restaurants <i class="fa fa-filter"></i></button>
       <div className="main-container">
-        {
-          restList?.map(restaurant => <RestaurantCard key={restaurant?.info?.id} restdata = {restaurant?.info}/>)
-        }
-        
-        
+        {restList?.map((restaurant) => (
+          <RestaurantCard
+            key={restaurant?.info?.id}
+            restdata={restaurant?.info}
+          />
+        ))}
       </div>
-    );
-  };
-  
+    </div>
+  );
+};
+
 export default MainContainer;
