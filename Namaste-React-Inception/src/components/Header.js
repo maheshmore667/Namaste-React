a18b84de5c2de88e15c4eef3975b3d90324c2754
Mahesh More <maheshmore667@gmail.com>
2023-11-25 16:42:02 +0530

    login/ logout button implemented

--- Namaste-React-Inception/src/components/Header.js
+++ Namaste-React-Inception/src/components/Header.js
@@ -1,6 +1,8 @@
 import logo from "../../logo.png";
+import {useState} from "react"
 
 const Header = () => {
+  const [btnName, setBtnName] = useState("Login")
     return (
       <div className="header-container">
         <div className="logo-container">
@@ -11,7 +13,9 @@ const Header = () => {
             <li>Home</li>
             <li>About</li>
             <li>Cart</li>
+            <button className="login-btn" onClick={()=>{btnName === "Login" ?setBtnName("Logout") : setBtnName("Login");}}>{btnName}</button>
           </ul>
+          
         </div>
       </div>
     );
