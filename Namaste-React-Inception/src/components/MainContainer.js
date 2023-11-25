e9a95ea6719e920e1f7620022efe36795696cec7
Mahesh More <maheshmore667@gmail.com>
2023-11-25 17:19:07 +0530

    search functionality implemented

--- Namaste-React-Inception/src/components/MainContainer.js
+++ Namaste-React-Inception/src/components/MainContainer.js
@@ -4,27 +4,67 @@ import ShimmerUI from "./ShimmerUI";
 
 const MainContainer = () => {
   const [restList, setRestList] = useState([]);
+  const [cpyRestList, setCpyRestList] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchRestaurantList();
   }, []);
 
-  const fetchRestaurantList = async() =>{
-    var data = await fetch("https://www.swiggy.com/mapi/homepage/getCards?lat=18.5538241&lng=73.9476689");
+  const fetchRestaurantList = async () => {
+    var data = await fetch(
+      "https://www.swiggy.com/mapi/homepage/getCards?lat=18.5538241&lng=73.9476689"
+    );
     data = await data.json();
-    setRestList(data?.data?.success?.cards[4]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
-  }
+    setRestList(
+      data?.data?.success?.cards[4]?.gridWidget?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+    setCpyRestList(
+      data?.data?.success?.cards[4]?.gridWidget?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+  };
 
   const filterRestList = () => {
-    setRestList(restList?.filter((res)=> res?.info?.avgRating > 4));
-  }
+    setRestList(restList?.filter((res) => res?.info?.avgRating > 4));
+  };
 
   //conditional rendering
-  return restList.length===0 ? <ShimmerUI /> : (
+  return restList.length === 0 ? (
+    <ShimmerUI />
+  ) : (
     <div className="body-content">
-      <button className="btn top-res" onClick={filterRestList}>Top Restaurants <i className="fa fa-filter"></i></button>
+      <div style={{ display: "flex", justifyContent: "end" }}>
+        <input
+          type="text"
+          style={{ borderRadius: "10px" }}
+          className="top-res"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button
+          className="btn"
+          style={{ marginTop: "10px" }}
+          onClick={() => {
+            setCpyRestList(
+              restList?.filter((resObj) => {
+                return resObj?.info?.name.toLowerCase().includes(searchText.toLowerCase());
+              })
+            );
+          }}
+        >
+          <i className="fa fa-search"></i>
+        </button>
+        <button className="btn top-res" onClick={filterRestList}>
+          Top Restaurants <i className="fa fa-filter"></i>
+        </button>
+      </div>
+
       <div className="main-container">
-        {restList?.map((restaurant) => (
+        {cpyRestList?.map((restaurant) => (
           <RestaurantCard
             key={restaurant?.info?.id}
             restdata={restaurant?.info}
