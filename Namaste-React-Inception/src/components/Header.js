6793d5286aee42dcb19601b4fbc210ac6df2ffef
Mahesh More <maheshmore667@gmail.com>
2023-12-02 20:54:58 +0530

    custom react hook is reused

--- Namaste-React-Inception/src/components/Header.js
+++ Namaste-React-Inception/src/components/Header.js
@@ -1,11 +1,12 @@
 import logo from "../../logo.png";
 import { useState} from "react"
 import { Link } from "react-router-dom";
+import useAvailableStatus from "../Utils/useAvailableStatus";
 
 const Header = () => {
   const [btnName, setBtnName] = useState("Login")
     return (
-      <div className="header-container">
+      <div className="header-container"> 
         <div className="logo-container">
           <img className="logo" src={logo} alt="alt-image" />
         </div>
@@ -14,7 +15,7 @@ const Header = () => {
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/cart">Cart</Link></li>
-            <button className="login-btn" onClick={()=>{btnName === "Login" ?setBtnName("Logout") : setBtnName("Login");}}>{btnName}</button>
+            <button className="login-btn" onClick={()=>{btnName === "Login" ?setBtnName("Logout") : setBtnName("Login");}}>{btnName} { useAvailableStatus() ? "✔" : "🚫"}</button>
           </ul>
           
         </div>
