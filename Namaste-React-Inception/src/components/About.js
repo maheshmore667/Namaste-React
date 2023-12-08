f579e62027738be1aa6fadf25cc7546b0b04509c
Mahesh More <maheshmore667@gmail.com>
2023-12-09 00:08:56 +0530

    css about page

--- Namaste-React-Inception/src/components/About.js
+++ Namaste-React-Inception/src/components/About.js
@@ -2,23 +2,25 @@ import "../Styles/About.css";
 import Mahesh from "../Assets/MAHESH.png";
 import SkillInfo from "./SkillInfo";
 
-const About = () =>{
-    return (
-        <section className="col-12 d-flex flex-column">
-        <div className="col-12" style={{"display" : "flex", "justifyContent" : "center"}}>
-          <img src={Mahesh} className="pic" alt=""/>
+const About = () => {
+
+  return (
+    <section className="about-section d-flex flex-column align-items-center">
+      <div className="profile-image-container my-4">
+        <img src={Mahesh} className="profile-image img-fluid" alt="Mahesh" />
+      </div>
+      <div className="about-text text-center my-2">
+        <h3 className="mb-3">About Me</h3>
+        <h5>MEAN && MERN Developer & <span>Technical Author</span></h5>
+        <p>Hi, I'm Mahesh More. With over 4 years of experience, I specialize in building Single Page Applications using the MEAN/MERN stack.</p>
+        <p>Sweet and simple intro, right?</p>
+        <div className="data">
+          {/* Add SkillInfo component or other content here if needed */}
         </div>
-        <div className="text col-12 text-center my-2">
-            <h3>About Me</h3>
-            <h5>MEAN && MERN Developer & <span>Technical Author</span></h5>
-              <p>My name is Mahesh More. having total strong experience of 4+ years building Single page application using (MEAN /MERN) stack.</p>
-              <p>Sweet and simple intro I guess!!</p>
-            <div className="data">
-            {/* <SkillInfo skill={"React"} excellency={"6"}/> */}
-            </div>
-          </div>
-      </section>
-      )
+      </div>
+    </section>
+  );
 }
+
 
 export default About;
